78034a5f957689786c987954173577bc4236e2f5
mugiwaLuffy <77626981+mugiwaLuffy@users.noreply.github.com>
2021-01-19 16:25:29 +0800

    Update help_me.py

--- chatrobot/plugins/help_me.py
+++ chatrobot/plugins/help_me.py
@@ -30,17 +30,17 @@ async def sedlyfsir(event):
     oknoob = Config.OWNER_ID
     oksir = Config.CUSTOM_START
     if Config.CUSTOM_START is None:
-        text_me = (f"**Hai. {firstname} , I am {bot_username}.** \n"
-               f"`I am A ChatBot To Talk With My` [Master](tg://user?id={oknoob}) \n"
-               f"**Send Me And I Will Send To Moi Master.** \n"
-               f"**Thank You**")
+        text_me = (f"**Hai. {firstname} , Saya {bot_username}.** \n"
+               f"`Saya seorang bot obrolan managed by` [Master](tg://user?id={oknoob}) \n"
+               f"**Kirim sesuatu yang anda sampaikan ke tuan saya.** \n"
+               f"**Terima kasih❤**")
     else:
         text_me = f"{oksir}"
-    formaster = "Sir. How Can I Help You?"
+    formaster = "Halo tuan, ada yang bisa saya bantu?"
     if event.sender_id == Config.OWNER_ID:
         ok = await chatbot.send_message(event.chat_id, message=formaster, buttons = [
-             [custom.Button.inline("Commands For Owner.", data="cmds")],
-             [custom.Button.inline("Close 🔐", data="close ")],
+             [custom.Button.inline("Perintah Untuk Owner.", data="cmds")],
+             [custom.Button.inline("Tutup 🔐", data="close ")],
               ]
              )
     else:
@@ -57,10 +57,10 @@ async def sedlyfsir(event):
 async def help(event):
     if event.query.user_id is not Config.OWNER_ID:
         await event.edit(
-            "You Can Deploy Your Own ChatBot By Watching Video Down There. \nThank You For Contacting Me.",
+            "Hai bosku kamu bisa join channel dan group. \nTerima kasih sudah menghubungi saya.",
             buttons=[
-                [Button.url("Deploy Tutorial 📺", "t.me/Infotel14")],
-                [Button.url("Need Help ❓", "t.me/SerenaAssistantBot")],
+                [Button.url("Channel Memes 📺", "t.me/MemesRpi")],
+                [Button.url("Group Fun ❓", "t.me/RPIrandompeopleindonesia")],
             ],
         )
         
